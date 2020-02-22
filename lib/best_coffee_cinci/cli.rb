1d3747dfebfba17b6a619e7e7a9577fe4a14b911
Marci Clark <marcig612@gmail.com>
2020-02-22 21:39:44 +0000

    added cli folder

---
+++ lib/best_coffee_cinci/cli.rb
@@ -0,0 +1,16 @@
+class BestCoffeeCinci::CLI 
+  
+  def call
+    puts "Need a jolt?  Here are Cincinnati's best coffee shops!"
+  end 
+  
+  def list_shops
+  end 
+  
+  def get_user_input
+  end 
+  
+  def get_shops_info
+  end 
+  
+end
