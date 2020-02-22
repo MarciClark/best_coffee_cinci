2c9c9c224c5ee20cfae94a8084156d6de5cf7d86
Marci Clark <marcig612@gmail.com>
2020-02-22 21:36:39 +0000

    first commit

---
+++ lib/best_coffee_cinci.rb
@@ -0,0 +1,6 @@
+require "best_coffee_cinci/version"
+
+module BestCoffeeCinci
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_coffee_cinci/version.rb
@@ -0,0 +1,3 @@
+module BestCoffeeCinci
+  VERSION = "0.1.0"
+end
