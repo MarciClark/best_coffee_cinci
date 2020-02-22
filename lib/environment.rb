85f5655d184f5739c9208bc6a1ea8c06012e55a0
Marci Clark <marcig612@gmail.com>
2020-02-22 21:43:09 +0000

    added requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,9 @@
 require "best_coffee_cinci/version"
+require_relative "./best_coffee_cinci/cli"
+require_relative "./best_coffee_cinci/scraper"
+
+require 'pry'
+require 'nokogiri'
 
 module BestCoffeeCinci
   class Error < StandardError; end
