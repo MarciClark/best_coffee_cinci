5d2011dc71850caa292d1b9bf77abc3d3083e65c
Marci Clark <marcig612@gmail.com>
2020-02-24 20:42:38 +0000

    modified requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,6 @@
-require "best_coffee_cinci/version"
-require_relative "./best_coffee_cinci/cli"
-require_relative "./best_coffee_cinci/scraper"
+require_relative "best_coffee_cinci/version"
+require_relative "best_coffee_cinci/cli"
+require_relative "best_coffee_cinci/scraper"
 
 require 'pry'
 require 'nokogiri'
@@ -9,3 +9,7 @@ module BestCoffeeCinci
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
+require_relative "london_spas/version"
+require_relative "london_spas/cli"
