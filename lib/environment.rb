1ed1804ef7089a37404b1224259cbf88ab17f385
Marci Clark <marcig612@gmail.com>
2020-02-25 19:47:36 +0000

    modified requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
-require_relative "best_coffee_cinci/version"
-require_relative "best_coffee_cinci/cli"
-require_relative "best_coffee_cinci/scraper"
+require_relative "./best_coffee_cinci/version"
+require_relative "./best_coffee_cinci/cli"
+require_relative "./best_coffee_cinci/scraper"
+require_relative "./best_coffee_cinci/shop_info"
+require_relative "./best_coffee_cinci/shop"
 
 require 'pry'
 require 'nokogiri'
