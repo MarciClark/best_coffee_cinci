b59be5fb94d767ad1eb51ced6dd3e6cdf9ed17bc
Marci Clark <marcig612@gmail.com>
2020-02-25 18:30:59 +0000

    modified call method

--- lib/best_coffee_cinci/cli.rb
+++ lib/best_coffee_cinci/cli.rb
@@ -2,6 +2,14 @@ class BestCoffeeCinci::CLI
   
   def call
     puts "Need a jolt?  Here are Cincinnati's best coffee shops!"
+    @input = ""
+    until @input == "exit"
+      get_shops 
+      list_shops
+      get_user_input
+      get_shop_info
+    end 
+    goodbye
   end 
   
   def list_shops
@@ -10,7 +18,7 @@ class BestCoffeeCinci::CLI
   def get_user_input
   end 
   
-  def get_shops_info
+  def get_shop_info
   end 
   
 end
