c7d808c7c6c26b93ffc901fb5bd7c132fb39d6e2
Marci Clark <marcig612@gmail.com>
2020-02-27 20:19:55 +0000

    fixed folders in env

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,9 @@
+require_relative "./best_coffee_cinci/version"
 require_relative "./best_coffee_cinci/cli"
 require_relative "./best_coffee_cinci/scraper"
 require_relative "./best_coffee_cinci/cli"
 require_relative "./best_coffee_cinci/shop_info"
-require_relative "./best_coffee_cinci/version"
+
 
 require 'pry'
 require 'nokogiri'
