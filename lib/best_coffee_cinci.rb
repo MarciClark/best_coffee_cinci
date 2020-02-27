8e4bf56563a7d918986766aa21238039f0fb8bd9
Marci Clark <marcig612@gmail.com>
2020-02-27 19:34:34 +0000

    added folder to lib

---
+++ lib/best_coffee_cinci.rb
@@ -0,0 +1,14 @@
+require_relative "./best_coffee_cinci/cli"
+require_relative "./best_coffee_cinci/scraper"
+require_relative "./best_coffee_cinci/cli"
+require_relative "./best_coffee_cinci/shop_info"
+require_relative "./best_coffee_cinci/version"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module EdenEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
