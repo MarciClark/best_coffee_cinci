4307d53aaed35a395bd4b284b6dcb3595a52c733
Marci Clark <marcig612@gmail.com>
2020-02-27 03:19:54 +0000

    testing requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,8 @@
-require_relative "./best_coffee_cinci/version"
-require_relative "./best_coffee_cinci/cli"
-require_relative "./best_coffee_cinci/scraper"
-require_relative "./best_coffee_cinci/shop_info"
-require_relative "./best_coffee_cinci/shop"
+require_relative '../lib/best_coffee_cinci/cli'
+require_relative '../lib/best_coffee_cinci/scraper'
+require_relative '../lib/best_coffee_cinci/shop_info'
+require_relative '../lib/best_coffee_cinci/shop'
+require_relative '../lib/nyt_bestsellers/version'
 
 require 'pry'
 require 'nokogiri'
