757bb8c0d2547076aa0c7863e2b86b35667501ce
Marci Clark <marcig612@gmail.com>
2020-02-27 14:20:53 +0000

    testing reqs

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-require_relative "../best_coffee_cinci/cli"
+require_relative "../lib/best_coffee_cinci/cli"
 require_relative "../lib/best_coffee_cinci/scraper"
 require_relative "../lib/best_coffee_cinci/shop_info"
 require_relative "../lib/best_coffee_cinci/shop"
--- lib/best_coffee_cinci/cli.rb
+++ lib/best_coffee_cinci/cli.rb
@@ -1,4 +1,4 @@
-class CLI 
+class BestCoffeeCinci::CLI 
   
   def call
     puts "Need a jolt?  Here are Cincinnati's best coffee shops!"
