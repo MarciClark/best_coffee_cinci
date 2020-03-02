1d8fec006263ad67b26fd29df30a800cd603ce99
Marci Clark <marcig612@gmail.com>
2020-03-02 17:01:33 +0000

    modified cli methods

--- lib/best_coffee_cinci/cli.rb
+++ lib/best_coffee_cinci/cli.rb
@@ -26,14 +26,14 @@ class BestCoffeeCinci::CLI
   
   def get_user_input
     user_shop = gets.strip.to_i
-    show_shops(user_shop) if valid_input(user_shop, @shops)
+    show_shop_info(user_shop) if valid_input(user_shop, @shops)
   end 
   
   def valid_input(input, data)
     input.to_i <= data.length && input.to_i > 0
   end 
   
-  def show_shops(user_shops)
+  def show_shop_info(user_shops)
     shop = @shops[user_shop - 1]
     shop.get_shop_info
     puts "Here is the info for #{shop.name}"
@@ -43,11 +43,8 @@ class BestCoffeeCinci::CLI
     get_user_input(shop)
   end 
   
-  def get_shop_info
-  end 
-  
-  def shop_info 
-  end 
+  # def get_shop_info
+  # end 
   
   def show_more
     puts "Ready to grab some coffee? Type 'exit' to exit or press any key to see more shops."
