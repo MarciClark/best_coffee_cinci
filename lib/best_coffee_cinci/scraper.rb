918146bd30addc4c2c6ce69510f516b912fc7a75
Marci Clark <marcig612@gmail.com>
2020-03-25 03:54:30 +0000

    saving

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -1,6 +1,8 @@
 class Scraper
   
-  def self.scrape_shops 
+  
+  
+  def self.shops 
     doc = Nokogiri::HTML(open("https://foursquare.com/top-places/cincinnati/best-coffee-shops"))
     
     shops = doc.css("p.venueName") 
