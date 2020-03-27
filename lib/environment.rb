4ca7523324c7b4ea6cc4b65ae9ce2e8139bb41ed
Marci Clark <marcig612@gmail.com>
2020-03-27 02:15:14 +0000

    fix requirement

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,7 +2,7 @@ require_relative "./best_coffee_cinci/version"
 require_relative "./best_coffee_cinci/cli"
 require_relative "./best_coffee_cinci/scraper"
 require_relative "./best_coffee_cinci/address"
-# require_relative "./best_coffee_cinci/shop_number"
+# require_relative "./best_coffee_cinci/number"
 
 
 require 'pry'
