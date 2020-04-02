4b83897f1cfd63d9424980a11a9514e10aaab5e4
Marci Clark <marcig612@gmail.com>
2020-04-02 18:56:48 +0000

    clean up scraper class

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -2,27 +2,10 @@ class BestCoffeeCinci::Scraper
   
   attr_accessor :shop, :address, :url
   
-  
-  # def self.all_shops
-  #   doc = Nokogiri::HTML(open("https://foursquare.com/top-places/cincinnati/best-coffee-shops"))
- 
-  #   shops = doc.css("p.venueName")
-  # end 
-  
-  # def self.scrape_address
-  #   doc = Nokogiri::HTML(open("https://foursquare.com/top-places/cincinnati/best-coffee-shops"))
-    
-  #   address = self.new 
-  #   shop_address = doc.css("div.address").text.gsub(/\t/, "")
-    
-  #   address
-  # end 
-  
   def self.all_shops
     doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
  
     shop = doc.css("h2.text-align:center")
-    # href = shop.css("a").first["href"]
   end 
   
   def self.scrape_address
