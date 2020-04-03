d54057bdf4eb8259a94bd78d36345dd74de8cdcd
Marci Clark <marcig612@gmail.com>
2020-04-03 03:41:12 +0000

    testing scraper

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -2,17 +2,32 @@ class BestCoffeeCinci::Scraper
   
   attr_accessor :shop, :address, :url
   
-  def self.all_shops
-    doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
+  # def self.all_shops
+  #   doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
  
-    shop = doc.css("h2.text-align:center")
+  #   shop = doc.css("h2.text-align:center")
+  # end 
+  
+  # def self.scrape_address
+  #   doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
+    
+  #   address = self.new 
+  #   shop_address = doc.css("br").text.gsub(/\t/, "")
+    
+  #   address
+  # end 
+  
+  def self.all_shops
+    doc = Nokogiri::HTML(open("https://twodrifters.us/blog/best-coffee-shops-in-cincinnati-ohio.html"))
+ 
+    shop = doc.css("h2.a.href")
   end 
   
   def self.scrape_address
-    doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
+    doc = Nokogiri::HTML(open("https://twodrifters.us/blog/best-coffee-shops-in-cincinnati-ohio.html"))
     
     address = self.new 
-    shop_address = doc.css("br").text.gsub(/\t/, "")
+    shop_address = doc.css("span.style").text.gsub(/\t/, "")
     
     address
   end 
