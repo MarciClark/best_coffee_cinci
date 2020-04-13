57c575688fb06c958db6b2994d673686b9ff0e4f
Marci Clark <marcig612@gmail.com>
2020-04-13 04:14:27 +0000

    testing scraper

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -2,25 +2,10 @@ class BestCoffeeCinci::Scraper
   
   attr_accessor :shop, :address, :url
   
-  # def self.all_shops
-  #   doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
- 
-  #   shop = doc.css("h2.text-align:center")
-  # end 
-  
-  # def self.scrape_address
-  #   doc = Nokogiri::HTML(open("https://club.atlascoffeeclub.com/cincinnati-coffee-shops-7-must-try/"))
-    
-  #   address = self.new 
-  #   shop_address = doc.css("br").text.gsub(/\t/, "")
-    
-  #   address
-  # end 
-  
   def self.all_shops
-    doc = Nokogiri::HTML(open("https://sprudge.com/a-coffee-drinkers-guide-to-cincinnati-141292.html"))
+    doc = Nokogiri::HTML(open("http://kematis.com/coffee-near-me/coffee-shops-in-cincinnati-ohio/"))
  
-    shop = doc.css("p.strong")
+    shop = doc.css("div.color#333333")
   end 
   
   def self.scrape_address
