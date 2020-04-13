d164ccd7c985b6b6b0162166b84e00a7d02d5cb2
Marci Clark <marcig612@gmail.com>
2020-04-13 05:25:33 +0000

    scraper test

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -2,19 +2,30 @@ class BestCoffeeCinci::Scraper
   
   attr_accessor :shop, :address, :url
   
+  # def self.all_shops
+  #   doc = Nokogiri::HTML(open("http://kematis.com/coffee-near-me/coffee-shops-in-cincinnati-ohio/"))
+ 
+  #   shop = doc.css("div.color#333333")
+  # end 
+  
+  # def self.scrape_address
+  #   doc = Nokogiri::HTML(open("https://twodrifters.us/blog/best-coffee-shops-in-cincinnati-ohio.html"))
+    
+  #   address = self.new 
+  #   shop_address = doc.css("div.arconix-box.arconix-box-gray").text.gsub(/\t/, "")
+    
+  #   address
+  # end 
+  
   def self.all_shops
     doc = Nokogiri::HTML(open("http://kematis.com/coffee-near-me/coffee-shops-in-cincinnati-ohio/"))
- 
-    shop = doc.css("div.color#333333")
+    shops = doc.css("td#align") 
+    
+    shops.each do |s|
+    name = s.text
+    ref = m.attr("value")
+    BestCoffeeCinci::Shop.new(name, ref)
+    end 
   end 
   
-  def self.scrape_address
-    doc = Nokogiri::HTML(open("https://twodrifters.us/blog/best-coffee-shops-in-cincinnati-ohio.html"))
-    
-    address = self.new 
-    shop_address = doc.css("div.arconix-box.arconix-box-gray").text.gsub(/\t/, "")
-    
-    address
-  end 
-  
-end
+end 
