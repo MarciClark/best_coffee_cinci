cada0e0c5283e67458696ed93b2299befd1e455c
Marci Clark <marcig612@gmail.com>
2020-04-18 03:29:17 +0000

    testing scraper

--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -1,12 +1,22 @@
 class BestCoffeeCinci::Scraper
   
-  def self.scrape_shops
-    doc = Nokogiri::HTML(open("https://www.uc.edu/campus-life/food/locations-hours/cafes.html"))
-    shops = doc.css(".component.title").drop(2).each do |shop|
-    name = shop.css(":first-child").text
-    BestCoffeeCinci::Shop.new(name)
-    end 
+  def self.scrape_shops 
+    url = 'https://www.uc.edu/campus-life/food/locations-hours/cafes.html'
+    html = open(url)
+    
+    doc = Nokogiri::HTML(html)
+    shops = []
+    doc.css(".component.title").drop(2).each do |shop|
   end 
+
+
+  # def self.scrape_shops
+  #   doc = Nokogiri::HTML(open("https://www.uc.edu/campus-life/food/locations-hours/cafes.html"))
+  #   shops = doc.css(".component.title").drop(2).each do |shop|
+  #   name = shop.css(":first-child").text
+  #   BestCoffeeCinci::Shop.new(name)
+  #   end 
+  # end
   
   # def self.scrape_addresses
   #   doc = Nokogiri::HTML(open("https://www.uc.edu/campus-life/food/locations-hours/cafes.html"))
