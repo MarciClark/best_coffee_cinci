078dbb0b8fdd1e468492f1a3066855f6e6562810
MarciClark <MarciG612@gmail.com>
2020-04-27 23:38:41 -0400

    modofiy scraper method

--- lib/best_coffee_cinci/location.rb
+++ lib/best_coffee_cinci/location.rb
@@ -1,23 +1,23 @@
-class Location
+# require 'pry'
+
+# class BestCoffeeCinci::Location
   
-  attr_accessor :name, :shops
+#   attr_accessor :name, :shops
   
-  @@all = []
+#   @@all = []
    
-  def initialize(name)
-    @name = name
-    @shops = []
-    save
-  end 
+#   def initialize(name)
+#     @name = name
+#     @shops = []
+#     save
+#   end 
   
-  def self.all 
-  end 
+#   def self.all 
+#     BestCoffeeCinci::Scraper.scrape_locations(self) if @@all.empty?
+#       @@all
+#   end 
   
-  def location
-    Scraper.locations(self) if @shops.empty?
-  end
-  
-  def save
-    @all << self 
-  end 
-end 
+#   def save
+#     @all << self 
+#   end 
+# end 
--- lib/best_coffee_cinci/scraper.rb
+++ lib/best_coffee_cinci/scraper.rb
@@ -18,7 +18,9 @@ class BestCoffeeCinci::Scraper
 
   def self.scrape_locations
     doc = Nokogiri::HTML(open("https://www.uc.edu/campus-life/food/locations-hours/cafes.html"))
-    locations = doc.css(".component.text br").text.strip
+    locations = doc.css(".component.text br").text.strip.split
+    #added .split to turn string into an array
+    binding.pry
     locations.each do |location|
       puts location.text
     end
